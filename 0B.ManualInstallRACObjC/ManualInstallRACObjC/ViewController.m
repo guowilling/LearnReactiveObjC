7cd0cc03326382fb0e9b3256ae02886be2a3e1c2
guowilling <guowilling90@gmail.com>
2017-02-24 19:17:24 +0800

    Update

--- 0B.ManualInstallRACObjC/ManualInstallRACObjC/ViewController.m
+++ 0B.ManualInstallRACObjC/ManualInstallRACObjC/ViewController.m
@@ -1,12 +1,13 @@
 //
 //  ViewController.m
-//  RACObjC
+//  ManualInstallRACObjC
 //
-//  Created by 郭伟林 on 17/2/10.
+//  Created by 郭伟林 on 17/2/15.
 //  Copyright © 2017年 SR. All rights reserved.
 //
 
 #import "ViewController.h"
+#import "ReactiveObjC.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //RACSignal
 }
 
 
